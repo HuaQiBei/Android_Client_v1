8e3ae058af320ea596ce4cba60765f106d6a4550
testfzx <2582876365@qq.com>
2016-08-05 12:05:11 +0800

    fzx test

--- app/src/main/java/com/bignerdranch/android/android_client_v1/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/android_client_v1/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private int a;
-    private int b;
+    private int c;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
