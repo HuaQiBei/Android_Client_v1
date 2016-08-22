92a98d3284f8953a0fde55c1e96c6a34b9bb84cd
Elvira <lixi.csu@gmail.com>
2016-08-22 19:40:38 +0800

    New changelist

--- app/src/main/java/com/bignerdranch/android/android_client_v1/ModifyEmailFragment.java
+++ app/src/main/java/com/bignerdranch/android/android_client_v1/ModifyEmailFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/bignerdranch/android/android_client_v1/PhoneFragment.java
+++ app/src/main/java/com/bignerdranch/android/android_client_v1/PhoneFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
