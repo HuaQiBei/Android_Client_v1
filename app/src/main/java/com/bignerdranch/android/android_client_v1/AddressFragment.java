adc13d7667e9aecd39c64d76521d91a1a8f2e769
testfzx <2582876365@qq.com>
2016-08-22 17:04:19 +0800

    fzhx

--- app/src/main/java/com/bignerdranch/android/android_client_v1/AddressFragment.java
+++ app/src/main/java/com/bignerdranch/android/android_client_v1/AddressFragment.java
@@ -13,7 +13,7 @@ public class AddressFragment extends Fragment {
     private View bt_addAddress;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_address,container,false);
-    bt_addAddress=v.findViewById(R.id.AddAddress);
+        bt_addAddress=v.findViewById(R.id.AddAddress);
 
         bt_addressSave=v.findViewById(R.id.addressSave);
         bt_addressSave.setOnClickListener(new View.OnClickListener(){
