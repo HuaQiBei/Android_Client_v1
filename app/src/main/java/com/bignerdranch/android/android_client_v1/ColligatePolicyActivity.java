d2019985db83a72d822ae3b4a1322ee3627d51d1
testfzx <2582876365@qq.com>
2016-09-15 15:27:00 +0800

    fzhx

---
+++ app/src/main/java/com/bignerdranch/android/android_client_v1/ColligatePolicyActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.android_client_v1;
+
+import android.support.v4.app.Fragment;
+
+public class ColligatePolicyActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new ColligatePolicyFragment();
+    }
+}
+
+
